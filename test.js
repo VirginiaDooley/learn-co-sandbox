4b5731fffe43b05977d4662e5bd460774fe43068
Virginia Dooley <vcdooley@gmail.com>
2020-11-03 18:43:08 +0000

    Automatically backed up by Learn

---
+++ test.js
@@ -0,0 +1,15 @@
+// declare the variable
+var word
+ 
+// assign a value to the variable
+word = 'bird'
+var word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
+
+var a = 5,  var b = 2,   var c = 3,  var d = 'hello',  var e = 'goodbye';
